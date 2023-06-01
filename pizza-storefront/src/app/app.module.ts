00905c3e5684cc213d9e0636d62395ddbd3a3500
infinitewonder <ravischenker1990@gmail.com>
2023-06-01 10:17:46 +0800

    Task 1

--- pizza-storefront/src/app/app.module.ts
+++ pizza-storefront/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main.component';
+import { OrdersComponent } from './components/orders/orders.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'orders/:email', component: OrdersComponent },
+  { path: '**', component: MainComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
-
-  providers: [ ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MainComponent, OrdersComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
